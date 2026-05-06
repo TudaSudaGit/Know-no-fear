b198832da76803e379399d1db8a801d3915e7db8
Eldar <eldarnezamutdinov2@gmail.com>
2026-05-06 08:23:11 +0700

    добавлено отслеживание камерой и убран разброс при стрельбе

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -2,19 +2,13 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    // Этот метод вызывается автоматически, когда пуля входит в триггер
     private void OnTriggerEnter2D(Collider2D hitInfo)
     {
-        // Проверяем, чтобы пуля не удалялась об самого игрока (если есть тег Player)
-        if (!hitInfo.CompareTag("Player"))
-        {
-            Destroy(gameObject); // Пуля исчезает при попадании
-        }
-    }
+        Destroy(gameObject);
 
-    // Дополнительно: удаление пули через 5 секунд, если она ни во что не попала
+    }
     private void Start()
     {
-        Destroy(gameObject, 5f);
+        Destroy(gameObject, 10f);
     }
 }
